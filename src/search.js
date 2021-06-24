5267ce2a99b3b46da4061704ed9af658d684c42f
tom-cheung <wai.tomcheung@gmail.com>
2021-06-23 23:13:12 -0700

    remove old search functionality

--- src/search.js
+++ src/search.js
@@ -5,33 +5,9 @@ const APIKEY = 'AIzaSyDwWl6oDb31K5tHyKcmmlNNHe1Njh4Relg';
 
 class Search {
     constructor(ReadingList, Menu) {
-        // this.titleInput = ''; 
-        // this.authorInput = ''; 
         this.results = []; 
         this.error = "";
-        // this.readingList = ReadingList;
-        // this.menu = Menu; 
- 
     }
-
-    // requestInput(question, choices=[]) {
-    //     let newPrompt = new Prompt(); 
-    //     newPrompt.promptUser(question, choices);
-    //     return newPrompt.userInput; 
-    // }
-
-    // performSearch() {
-    //     console.log('Please provide a title and/or author to search by:')
-    //     this.titleInput = this.requestInput('Search by book title:');
-    //     this.authorInput = this.requestInput('Search by author:');
-
-    //     if(this.titleInput === '' && this.authorInput === '') {
-    //         console.log('Either title or author input is required!');
-    //         this.performSearch();
-    //     } else {
-    //         this.searchBook(this.titleInput, this.authorInput)
-    //     }
-    // }
 
     async searchBook(title='', author='') {
         try {
@@ -46,13 +22,8 @@ class Search {
                         let book = searchResults.data.items[i];
                         if(book) this.results.push(book);
                     }
-
-                    // console.log('\n**********Your search results**********\n');
-                    // this.displayResults(); 
-                    // this.saveBook(); 
                 } 
             } else {
-                // console.log('could not reach Google Books API!!!')
                 this.error = 'could not reach Google books API!!!'; 
             }
         }
@@ -61,58 +32,6 @@ class Search {
             console.log(err)
         }
     }
-
-    // displayResults() {
-    //     if(this.results.length > 0) {
-    //         for(let i = 0; i < this.results.length; i++) {
-    //             let {volumeInfo: {title, authors, publisher}} = this.results[i];
-    //             console.log(`Result: [${i + 1}]`);
-    //             console.log(`Title: ${title ? title : 'unavailable'}`);
-    //             console.log(`Author(s): ${authors ? (authors.length > 0 ? authors.join(", ") : author[0]) : 'unavailable'}`)
-    //             console.log(`Publisher: ${publisher ? publisher : 'unavailable'}`)
-    //             console.log(`\n----------\n`)
-    //         }
-    //     } else {
-    //         console.log('no results to display')
-    //     }
-    // }
-
-    // saveBook() {
-    //     let newPrompt = new Prompt();
-    //     let bookIds = newPrompt.promptUser('Enter the result # of the books you want to save, i.e. 1 2 3, OR \'quit\' to return to the main menu to view your list or perform a new search.'); 
-    //     let formattedIds = bookIds.split(' ');
-    //     let matchingIds = new Set(); 
-
-    //     if(bookIds === 'quit') {
-    //         this.menu.mainMenu(); 
-    //     } else if(formattedIds.length > 0) {
-
-    //         for(let id of formattedIds) {
-    //             let intId = parseInt(id) - 1;
-    //             if(intId >= 0 && intId < this.results.length ) {
-    //                 matchingIds.add(intId)
-    //             }
-    //         }
-
-
-    //         if(Array.from(matchingIds).length > 0) {
-    //             for(let id of matchingIds) {
-    //                 if(this.readingList.collection[this.results[id].id]) {
-    //                     console.log(`${this.results[id].volumeInfo.title} has already been saved.`)
-    //                 } else {
-    //                     this.readingList.addBook(this.results[id]);
-    //                 }
-                    
-    //             }
-    //             console.log('\nAdded books to your list, return to main menu to see your list\n')
-    //             this.saveBook(); 
-    //         } else {
-    //             console.log('\nThe result #\'s you provided did not match the search results.\n')
-    //             this.saveBook(); 
-    //         }
-    //     }
-
-    // }
 }
 
 module.exports = Search; 
